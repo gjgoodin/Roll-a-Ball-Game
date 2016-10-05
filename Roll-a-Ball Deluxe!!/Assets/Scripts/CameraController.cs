83e052eacae60062392d22fee4dfc5288092bb0a
gjgoodin <gjgoodin@indiana.edu>
2016-10-05 16:14:37 -0400

    Post Player Test
    Got everything working within the new directory. Nothing new from old
    commit, just everything works as it did before moving the files to the
    github directory

--- Roll-a-Ball Deluxe!!/Assets/Scripts/CameraController.cs
+++ Roll-a-Ball Deluxe!!/Assets/Scripts/CameraController.cs
@@ -3,64 +3,64 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
-	public Transform target;
-	public float angularSpeed;
-
-	[SerializeField][HideInInspector] 
-	private Vector3 initialOffset;
-
-	private Vector3 currentOffset;
-
-	[ContextMenu("Set Current Offset")]
-	private void SetCurrentOffset () {
-		if(target == null) {
-			return;
-		}
-
-		initialOffset = transform.position - target.position;
-	}
-
-	private void Start () {
-		if(target == null) {
-			Debug.LogError ("Assign a target for the camera in Unity's inspector");
-		}
-
-		currentOffset = initialOffset;
-	}
-
-	private void LateUpdate () {
-		transform.position = target.position + currentOffset;
-
-		float movement = Input.GetAxis ("Horizontal") * angularSpeed * Time.deltaTime;
-		if(!Mathf.Approximately (movement, 0f)) {
-			transform.RotateAround (target.position, Vector3.up, movement);
-			currentOffset = transform.position - target.position;
-		}
-	}
-
-//	public GameObject player;
-//	public float turnSpeed;
-//	public Transform player_cam;
-//	private Vector3 offset;
+//	public Transform target;
+//	public float angularSpeed;
 //
-//	// Use this for initialization
-//	void Start () {
-//		offset = transform.position - player.transform.position;
+//	[SerializeField][HideInInspector] 
+//	private Vector3 initialOffset;
 //
-//	}
-//	
-//	// Update is called once per frame
-//	void LateUpdate () {
-//		transform.position = player.transform.position + offset;
+//	private Vector3 currentOffset;
 //
-//		float movement = Input.GetAxis ("Horizontal_1") * turnSpeed * Time.deltaTime;
-//		if(!Mathf.Approximately (movement, 0f)) {
-//			transform.RotateAround (player_cam.position, Vector3.up, movement);
-//			offset = transform.position - player_cam.position;
+//	[ContextMenu("Set Current Offset")]
+//	private void SetCurrentOffset () {
+//		if(target == null) {
+//			return;
 //		}
 //
-//		offset = Quaternion.AngleAxis (Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
-//		transform.position = player_cam.position + offset; 
-//		transform.LookAt(player_cam.position);
+//		initialOffset = transform.position - target.position;
 //	}
+//
+//	private void Start () {
+//		if(target == null) {
+//			Debug.LogError ("Assign a target for the camera in Unity's inspector");
+//		}
+//
+//		currentOffset = initialOffset;
+//	}
+//
+//	private void LateUpdate () {
+//		transform.position = target.position + currentOffset;
+//
+//		float movement = Input.GetAxis ("Horizontal") * angularSpeed * Time.deltaTime;
+//		if(!Mathf.Approximately (movement, 0f)) {
+//			transform.RotateAround (target.position, Vector3.up, movement);
+//			currentOffset = transform.position - target.position;
+//		}
+//	}
+
+	public GameObject player;
+	public float turnSpeed;
+	public Transform player_cam;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+
+		float movement = Input.GetAxis ("Horizontal_1") * turnSpeed * Time.deltaTime;
+		if(!Mathf.Approximately (movement, 0f)) {
+			transform.RotateAround (player_cam.position, Vector3.up, movement);
+			offset = transform.position - player_cam.position;
+		}
+
+		offset = Quaternion.AngleAxis (Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+		transform.position = player_cam.position + offset; 
+		transform.LookAt(player_cam.position);
+	}
 }
